74122d8bf89052e3d1f601dc8be436735ca37dc9
Pawel Socha <pawel.socha@microsoft.wsei.edu.pl>
2022-04-20 11:32:07 +0200

    kk

--- pab1/src/index.js
+++ pab1/src/index.js
@@ -3,29 +3,6 @@ var app = express();
  app.get('/', function (req, res) {
   res.send('Hello World');
  
-const operator = prompt('Podaj operator: ');
-const num1 = parseFloat(prompt('Podaj pierwszy numer: '));
-const num2 = parseFloat(prompt('Podaj drugi numer: '));
 
-let wynik;
-
-
-if (operator == '+') {
-    wynik = num1 + num2;
-}
-else if (operator == '-') {
-    wynik = num1 - num2;
-}
-else if (operator == '*') {
-    wynik = num1 * num2;
-}
-else {
-    wynik = num1 / num2;
-}
-
-console.log(`${num1} ${operator} ${num2} = ${wynik}`)
-  
-  
- });
   app.listen(3000);
-
+ })
--- pab2/index.ts
+++ pab2/index.ts
@@ -1,3 +1,4 @@
+
 import express from 'express'
 import {Request, Response} from 'express'
 
@@ -12,21 +13,5 @@ app.post('/', function (req: Request, res: Response) {
   console.log(req.body) // e.x. req.body.title 
   res.status(200).send('POST Hello World')
 })
-export interface Note {
-    title: string; 
-    content: string;
-    createDate?:Date;
-    tags?: string;
-    id?: number;
-}
-
-const getPost = async (req: Request, res: Response) => {
-    
-    let id: string = req.params.id;
-    let post: Request = req.data;
-    return res.status(200).json({
-        message: post
-    });
-};
 
 app.listen(3000)
