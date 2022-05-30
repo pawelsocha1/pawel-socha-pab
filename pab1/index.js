0bd02020984e0d9ecf7d6e57877d77006c10fcfa
Pawel Socha <pvwxlsocha@gmail.com>
2022-05-30 23:21:18 +0200

    :((

--- pab1/index.js
+++ pab1/index.js
@@ -3,4 +3,9 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World');
 });
+app.get('/add/:int1/:int2', function (req, res) {
+    var num1 = req.params.int1;
+    var num2 = req.params.int2;
+    res.send(num1 + num2);
+});
 app.listen(3000);
